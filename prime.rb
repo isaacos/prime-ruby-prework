dedcf41c93c9354b71ab804135d4d81db522150c
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-16 03:54:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,16 @@ def prime? (num)
   range = (1..num)
   new_array = []
   
-  range.each do 
+  range.each do |item|
+    
+    if num % item == 0 
+      new_array << item 
+    end
+    
+  end
+  
+  if new_array.size > 2 return false 
+  else return true 
+  end
   
 end
